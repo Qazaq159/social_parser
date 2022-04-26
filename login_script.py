b849b3ffa1f394277d3511f0ce7213b52f88df70
Qwerty Qazaq <zat.tyme.su@gmail.com>
2022-04-27 05:53:03 +0600

    2022.04.27 5:53

--- login_script.py
+++ login_script.py
@@ -1,11 +1,15 @@
 import csv
 from time import sleep
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 import selenium
 from selenium.webdriver.common.by import By
+from pyvirtualdisplay import Display
 
-driver = selenium.webdriver.Chrome('drivers/chromedriver')
+vdisplay = Display(visible=0, size=(1024, 768)) # if set visible value to 0, it will not show display
+vdisplay.start()
+
+option = selenium.webdriver.ChromeOptions().add_argument('headless')
+driver = selenium.webdriver.Chrome('drivers/chromedriver', options=option)
 
 driver.get('https://twitter.com/login')
 
@@ -16,21 +20,26 @@ username.send_keys(Keys.RETURN)
 
 
 def write_password(driver):
+    sleep(1.5)
     password = driver.find_element(by=By.XPATH,
                                    value='//input[@autocomplete="current-password"]')
     password.send_keys('Twitter2022$')
     password.send_keys(Keys.RETURN)
 
 
-sleep(0.5)
 try:
     write_password(driver)
+    sleep(2)
+
+    vdisplay.stop()
 except:
     login = driver.find_element(by=By.XPATH,
                                 value='//input[@autocomplete="on"]')
     login.send_keys('ferkin_kz')
     login.send_keys(Keys.RETURN)
-
     sleep(1)
 
     write_password(driver)
+    sleep(2)
+
+    vdisplay.stop()

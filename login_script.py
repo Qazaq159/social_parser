debf945b9f8ec4d00927c087ef45f016aca3135d
Qwerty Qazaq <zat.tyme.su@gmail.com>
2022-04-30 18:21:42 +0600

    gg

--- login_script.py
+++ login_script.py
@@ -5,46 +5,45 @@ import selenium
 from selenium.webdriver.common.by import By
 from pyvirtualdisplay import Display
 
+def write_password(driver, password_):
+    sleep(1.5)
+    password = driver.find_element(by=By.XPATH,
+                                   value='//input[@autocomplete="current-password"]')
+    password.send_keys(password_)
+    password.send_keys(Keys.RETURN)
+
+
 vdisplay = Display(visible=1, size=(1024, 768)) # if set visible value to 0, it will not show display
 vdisplay.start()
 
 option = selenium.webdriver.ChromeOptions().add_argument('headless')
 driver = selenium.webdriver.Chrome('drivers/chromedriver', options=option)
-
 driver.get('https://twitter.com/login')
+
+user = 'ferkin_kz'
+email = 'zat.tyme.su@gmail.com'
+password = 'Twitter2022$'
 
 sleep(2.5)
 username = driver.find_element(by=By.XPATH, value='//input[@autocomplete="username"]')
-username.send_keys('zat.tyme.su@gmail.com')
+username.send_keys(email)
 username.send_keys(Keys.RETURN)
 
-
-def write_password(driver):
-    sleep(1.5)
-    password = driver.find_element(by=By.XPATH,
-                                   value='//input[@autocomplete="current-password"]')
-    password.send_keys('Twitter2022$')
-    password.send_keys(Keys.RETURN)
-
-
 try:
-    write_password(driver)
+    write_password(driver, password)
     sleep(2)
-
 
 except:
     login = driver.find_element(by=By.XPATH,
                                 value='//input[@autocomplete="on"]')
-    login.send_keys('ferkin_kz')
+    login.send_keys(user)
     login.send_keys(Keys.RETURN)
     sleep(1)
 
-    write_password(driver)
+    write_password(driver, password)
     sleep(2)
 
 driver.get('https://twitter.com/elonmusk')
 sleep(1.5)
-try:
-    tweets = driver.find_element(by=By.XPATH, value='/article')
-except Exception:
-    print('0')
+
+
